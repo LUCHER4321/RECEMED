78cc6292d369cdae43238fcaad55d6332a98d32f
LUCHER4321 <lucher4321@gmail.com>
2024-08-09 18:59:33 -0400

    Update Prescription.jsx

--- src/assets/Prescription.jsx
+++ src/assets/Prescription.jsx
@@ -1,4 +1,4 @@
-export function prescription(profile){
+export function prescription(profile, token = ""){
     return(
         <header className="absolute top-0 right-0">{profile.first_name} {profile.last_name}</header>
     );
