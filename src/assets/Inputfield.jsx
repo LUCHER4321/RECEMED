cac766a106eda867220368fcef04da87314afef9
LUCHER4321 <lucher4321@gmail.com>
2024-08-10 23:21:30 -0400

    Delete Inputfield.jsx

--- src/assets/Inputfield.jsx
+++
@@ -1,5 +0,0 @@
-export function inputField(placeholder, value, setValue) {
-    return(
-        <input type="text" placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)}/>
-    );
-}
