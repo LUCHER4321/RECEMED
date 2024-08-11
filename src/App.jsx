052002a3ccacb7746718dd38cd651a7999389694
LUCHER4321 <lucher4321@gmail.com>
2024-08-10 23:20:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { testRUT } from './assets/TestRUT';
-import { page } from './assets/Page';
-import { requestData } from './assets/RequestData';
-import { prescription } from './assets/Prescription';
 
 function App() {
-  const [RUT, setRUT] = useState('');
-  const [pass, setPass] = useState('');
-  const [currentPage, setCurrentPage] = useState('RUTPage');
-  const [prescriptions, setPrescriptions] = useState(null);
-  const nextButton = () => {
-      const valid = testRUT(RUT);
-      if (valid) {
-          setCurrentPage('passPage');
-      } else {
-          alert("RUT inválido");
-      }
-  };
-  const login = async () => {
-    const response = await requestData(RUT, pass);
-    if(response.error){
-      alert("Contraseña incorrecta");
-    }
-    else{
-      setCurrentPage('');
-      const profile = response.data.profiles[0];
-      const token = response.data.token;
-      setPrescriptions(await prescription(profile, token));
-    }
-  };
-  const RUTPage = page("Ingresa tu RUT", RUT, setRUT, "Siguiente", nextButton); // RUT = 11111111-1
-  const passPage = page("Ingresa tu contraseña", pass, setPass, "Ingresar", login); // PASS = 11223344
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex flex-col items-center relative">
-      {currentPage === 'RUTPage' && RUTPage}
-      {currentPage === 'passPage' && passPage}
-      {prescriptions}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
